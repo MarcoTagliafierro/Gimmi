db65511f21bbcbea7f43e6cf29e1d7f9da14f575
Marco Tagliafierro <tagliafierro.marco@gmail.com>
2022-08-25 16:30:41 +0200

    Initial Commit

---
+++ Gimmi_git_safe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gimmi_git_safe
+//
+//  Created by Marco Tagliafierro on 25/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gimmi_git_safe/Gimmi_git_safeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gimmi_git_safeApp.swift
+//  Gimmi_git_safe
+//
+//  Created by Marco Tagliafierro on 25/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Gimmi_git_safeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
